2274a12cc7cd7056fc1051d84f5211157719503e
Lucky2307 <alana.luki@gmail.com>
2022-06-18 00:06:52 +0700

    added dropdown

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
-class DropdownItem extends React.Component {
-    render() {
-        return
-    }
+import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+
+function DropdownItem(props) {
+    return (
+        <li className='dropdownItem'>
+            <button className='dropdownBtn dropdownItemBtn' onClick={() => {
+                props.setSelected(props.children);
+                props.setOpen(false);
+                console.log(props.algoId);
+            }}>{props.children}</button>
+        </li>
+    )
 }
 
 class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: 'Reverse Linked List',
+            open: false,
+        }
+    }
+    setSelected(name) {
+        this.setState({ selected: name })
+    }
+    setOpen(b) {
+        this.setState({ open: b })
+    }
+
     render() {
-        return
+        return (
+            <div
+                className='dropdownContainer'
+                onBlur={(e) => {
+                    if (!e.currentTarget.contains(e.relatedTarget)) {
+                        // Won't run if focused target is of child of current focus
+                        this.setOpen(false);
+                    }
+                }}
+            >
+                <button
+                    className='dropdownBtn'
+                    onClick={() => this.setOpen(true)}
+                    setSelected={(name) => this.setSelected(name)}
+                >{this.state.selected}<FontAwesomeIcon className='dropdownIcon' icon={faCaretDown} /></button>
+                {this.state.open ?
+                    <div className='dropdownMenu'>
+                        <ul>
+                            {// Add setSelected function to each children props
+                                React.Children.map(this.props.children, child => {
+                                    return React.cloneElement(child, { setSelected: (name) => this.setSelected(name), setOpen: (b) => this.setOpen(b) })
+                                })}
+                        </ul>
+                    </div>
+                    : ''}
+
+            </div>
+        )
     }
 }
 
@@ -18,9 +66,11 @@ function Navbar() {
     return <nav className="nav">
         <a href="/" className="navTitle">Algorithm Visualization</a>
         <button className='runBtn'>Run!</button>
-        <div className='dropdownContainer'>
-            <button className='dropdownBtn'>Reverse Linked List <FontAwesomeIcon icon={faCaretDown} /></button>
-        </div>
+        <Dropdown>
+            <DropdownItem algoId="reverselink">Rev</DropdownItem>
+            <DropdownItem algoId="tree">Treeeeeeeeeeeee</DropdownItem>
+            <DropdownItem algoId="tree">Treeeeeeeeeeeee</DropdownItem>
+        </Dropdown>
     </nav>
 }
 
